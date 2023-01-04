ab1e892a0216fb42ed3987a4914efedf1e044d4b
Renan Viana <renanvianalc@gmail.com>
2023-01-04 10:46:05 -0300

    Updates

---
+++ precache-manifest.fffc3aa1f59266e8b25810e48f9e54e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c613cc1925ad1972103",
+    "url": "/react-gh-pages/static/css/main.0a8e392f.chunk.css"
+  },
+  {
+    "revision": "5c613cc1925ad1972103",
+    "url": "/react-gh-pages/static/js/main.5c613cc1.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react-gh-pages/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/react-gh-pages/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "a6558f0c85422d431291",
+    "url": "/react-gh-pages/static/js/runtime~main.a6558f0c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "aa72debfd6fa7f91b5e7f22bf81c8dda",
+    "url": "/react-gh-pages/index.html"
+  }
+];
